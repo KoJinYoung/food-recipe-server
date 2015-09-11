87c57ab8a80b8101fda585e00a53436d441a6cb4
zilla <zilla@maksse.com>
2015-09-11 21:39:32 +0900

    add error files

---
+++ Recipe.thrift
@@ -0,0 +1,58 @@
+namespace java com.foodRecipe.core.thrift.model.recipe
+namespace cocoa makcipeAPI
+
+////////////////////////////////////////////////////////////////////////
+//
+// define c style integer
+
+typedef i64 long
+typedef i32 int
+
+enum RECIPETYPE{
+	NONE = 0,
+	RECOMM = 1,
+	SUBSC = 2,
+}
+
+enum LEVEL{
+	EASY = 1,
+	NORMAL = 2,
+	HARD = 3
+}
+
+struct IngredientInfo{
+        1:      optional string name;
+        2:      optional string id;
+}
+
+struct RecipeInfo{
+	1: 	optional int calories;
+	2: 	optional LEVEL level;
+	3:	optional int cookTime;
+	4:	optional list<IngredientInfo>	ingredient;
+}
+
+struct Recipe{
+	1:	optional int recipeId;
+	2:	optional int writerId;
+	3:	optional string writerPic;
+	4:	optional string writerName;
+	5:	optional string recipeName;
+	6:	optional string recipePic;
+	7:	optional string	recipeComment;
+	8:	optional RecipeInfo	recipeInfo;
+	9:	optional bool recipeLiked;
+	10:	optional RECIPETYPE	recipeType;
+}	
+
+enum RecipeExCode{
+	INVALID = 0,
+	SERVER_ERROR = 1,
+	NOT_FOUND = 2,
+	DATA_EXIST = 3,
+}
+
+exception RecipeException{
+	1:	RecipeExCode what;
+	2:	string why;
+}
