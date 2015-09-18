9d95bbbd1972a500e7a82d39cf79bf136a1391b0
zilla <zilla@maksse.com>
2015-09-18 16:03:58 +0900

    remove all

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.createServer(function(req, res){
-	res.writeHead(200);
-	res.write("Server is running... :)");
-	res.end();
-}).listen(8080);
-
-console.log('Listening on port 8080...');
